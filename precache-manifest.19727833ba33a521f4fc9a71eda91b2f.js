52c6c51ec7509ffd848e2c8d62a74178c07e17f4
Clifton West Jr <cliftonwestjr@gmail.com>
2020-02-04 09:56:36 -0500

    Updates

---
+++ precache-manifest.19727833ba33a521f4fc9a71eda91b2f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "db61239f1e02ce523dd8e264ef2fcbe7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "56b7afffa429e0262821",
+    "url": "/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "f7a7be7825c394c568cb",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "56b7afffa429e0262821",
+    "url": "/static/js/2.fa623824.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.fa623824.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f7a7be7825c394c568cb",
+    "url": "/static/js/main.467c3d15.chunk.js"
+  },
+  {
+    "revision": "1d895283d22effa6eb3d",
+    "url": "/static/js/runtime-main.0eabbf88.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
